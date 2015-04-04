e27ee293d72a5b7824d6fa1762b322f6aa4db0f6
Michael Lavers <kolanos@gmail.com>
2015-04-04 14:58:02 -0700

    renaming the fixture functions into more maningful names

--- nhlstats/fixtures.py
+++ nhlstats/fixtures.py
@@ -25,10 +25,10 @@ def connect_dataset():
     return DataSet(os.environ.get('DATABASE_URL') or DEFAULT_SQLITE_DB)
 
 
-def freeze(basedir=None, format='json'):
+def dump(basedir=None, format='json'):
     """Dump the data stored in the database into a series of fixtures."""
     ds = connect_dataset()
-    logger.info('Freezing the database into fixtures...')
+    logger.info('Dumping the database into fixtures...')
     if not basedir:
         basedir = FIXTURES_DIR
     logger.info('Using {} as the fixtures directory...'.format(basedir))
@@ -38,15 +38,15 @@ def freeze(basedir=None, format='json'):
     for name in ds.tables:
         table = ds[name]
         filename = os.path.join(basedir, '{}.{}'.format(name, format))
-        logger.info('Freezing fixture for {} in {}...'.format(name, filename))
+        logger.info('Dumping fixture for {} in {}...'.format(name, filename))
         ds.freeze(table.all(), format=format, filename=filename)
 
 
-def thaw(basedir=None):
+def load(basedir=None):
     """Load data from a series of fixtures into the database."""
 
     ds = connect_dataset()
-    logger.info('Thawing data from fixtures and loading into database...')
+    logger.info('Loading data from fixtures into database...')
     if not basedir:
         basedir = FIXTURES_DIR
     if not os.path.isdir(basedir):
@@ -69,5 +69,5 @@ def thaw(basedir=None):
                 table))
             continue
         table = ds[table]
-        logger.info('Thawing {} into {} table...'.format(fixture, table))
+        logger.info('Loading {} into {} table...'.format(fixture, table))
         table.thaw(format=format, filename=filename, strict=True)
